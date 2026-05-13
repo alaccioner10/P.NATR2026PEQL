387cdc7e2535e63fc71d2bf893a7f4a95b033eb8
Alaccioner <alaccioner46@gmail.com>
2026-05-12 21:42:22 -0300

    soy pelotudo

--- Tp/SGE/SGE.Aplicacion/Expedientes/DTOs/EliminarExpedienteRequest.cs
+++ Tp/SGE/SGE.Aplicacion/Expedientes/DTOs/EliminarExpedienteRequest.cs
@@ -1,3 +1,5 @@
 namespace SGE.Aplicacion.Expedientes.DTOs;
 
 public record EliminarExpedienteRequest(Guid ExpedienteId, Guid IdUsuario);
+
+public record class EliminarExpedienteResponse(Guid Id, string Mensaje);
--- Tp/SGE/SGE.Aplicacion/Expedientes/UseCases/EliminarExpedienteUseCase.cs
+++ Tp/SGE/SGE.Aplicacion/Expedientes/UseCases/EliminarExpedienteUseCase.cs
@@ -1,43 +1,42 @@
 using SGE.Aplicacion.Autorizacion;
+using SGE.Aplicacion.Expedientes.DTOs;
 using SGE.Aplicacion.Tramites;
-using SGE.Aplicacion.Expedientes.DTOs; 
-using SGE.Aplicacion.Expedientes;     
 
 namespace SGE.Aplicacion.Expedientes.UseCases;
 
 public class EliminarExpedienteUseCase
 {
-    private readonly IExpedienteRepository _repoExpediente;
-    private readonly ITramiteRepository _repoTramite;
+    private readonly IExpedienteRepository _iExpRepo;
+    private readonly ITramiteRepository _iTramRepo;
     private readonly IAutorizacionService _autorizacion;
 
     public EliminarExpedienteUseCase(
-        IExpedienteRepository repoExpediente,
-        ITramiteRepository repoTramite,
-        IAutorizacionService autorizacion
-    )
+        IExpedienteRepository iExpRepo, 
+        ITramiteRepository iTramRepo, 
+        IAutorizacionService autorizacion)
     {
-        _repoExpediente = repoExpediente;
-        _repoTramite = repoTramite;
+        _iExpRepo = iExpRepo;
+        _iTramRepo = iTramRepo;
         _autorizacion = autorizacion;
     }
 
-    public void Ejecutar(EliminarExpedienteRequest request)
+    public EliminarExpedienteResponse Ejecutar(EliminarExpedienteRequest req)
     {
 
-        if (!_autorizacion.PoseeElPermiso(request.IdUsuario, Permiso.ExpedienteBaja))
+        if (!_autorizacion.PoseeElPermiso(req.IdUsuario, Permiso.ExpedienteBaja))
         {
-            throw new AutorizacionException("El usuario no tiene permisos para eliminar expedientes.");
+            throw new AutorizacionException("El usuario no tiene permisos para eliminar este expediente.");
         }
 
-
-        var tramitesDelExpediente = _repoTramite.ObtenerPorExpedienteId(request.ExpedienteId);
-
-        foreach (var tramite in tramitesDelExpediente)
+        var tramites = _iTramRepo.ObtenerPorExpedienteId(req.ExpedienteId);
+        foreach (var t in tramites)
         {
-            _repoTramite.Eliminar(tramite.Id);
+            _iTramRepo.Eliminar(t.Id);
         }
 
-        _repoExpediente.Eliminar(request.ExpedienteId);
+        _iExpRepo.Eliminar(req.ExpedienteId);
+
+
+        return new EliminarExpedienteResponse(req.ExpedienteId, "Expediente y sus trámites eliminados con éxito.");
     }
 }
